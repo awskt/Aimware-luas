a09c8269c4751742a71bbd1cc8093b76f4f53e2a
iSunOfficial <38258637+iSunOfficial@users.noreply.github.com>
2020-04-25 23:33:42 +0500

    Added crosshair marker

--- HealthshotHitmarker.lua
+++ HealthshotHitmarker.lua
@@ -21,22 +21,31 @@ local msc_ref = gui.Groupbox(ref, "Healthshot hitsound", 328,313, 296,400)
 local allenabled = gui.Checkbox(msc_ref, 'lua_healthshot_hitsound_enabled', 'Hitsound/marker lua enabled', 1)
 local killsay = gui.Checkbox(msc_ref, 'lua_healthshot_killsay_enabled', 'Enable killsay', 0)
 killsay:SetDescription("Writing random messages on kill")
+local hitcross = gui.Checkbox(msc_ref, 'lua_healthshot_hitcross_enabled', 'Enable crosshair marker', 0)
+local hitmarkerColor = gui.ColorPicker(hitcross, "lua_healthshot_hitcross_color", "", 50, 10, 255, 255);
+hitcross:SetDescription("Drawing a marker on the center of screen") 
+local linesize = gui.Slider(msc_ref, 'lua_healthshot_hitcross_slider', 'Crosshair marker size', 15, 0, 20)
+
 local killssounds = gui.Combobox(msc_ref, 'lua_healthshot_killsound_combobox', 'Killsound', "Off", "Bass", "Battlefield 4", "Bowhit", "Bruh", "You a gay", "Kovaak", "Cookie", "Windows XP error", "Roblox", "Vitas", "Minecraft hit", "Gachimuchi", "Metro 2033", "Minecraft oh", "Rust HS", "Toy", "Impact (loud)")
 killssounds:SetDescription("Overrides the headshot and hit sound on kill")
 local hssounds = gui.Combobox(msc_ref, 'lua_healthshot_hssounds_combobox', 'Headshot sound', "Off", "Bass", "Battlefield 4", "Bowhit", "Bruh", "You a gay", "Kovaak", "Cookie", "Windows XP error", "Roblox", "Vitas", "Minecraft hit", "Gachimuchi", "Metro 2033", "Minecraft oh", "Rust HS", "Toy", "Impact")
 hssounds:SetDescription("Overrides the hitsound on HS")
 local hitssounds = gui.Combobox(msc_ref, 'lua_healthshot_hitsound_combobox', 'Hitsound', "Off", "Bass", "Battlefield 4", "Bowhit", "Bruh", "You a gay", "Kovaak", "Cookie", "Windows XP error", "Roblox", "Vitas", "Minecraft hit", "Gachimuchi", "Metro 2033", "Minecraft oh", "Rust HS", "Toy", "Impact")
-
 local Healthshot = gui.Combobox(msc_ref, 'lua_healthshot_hitmarker_combobox', 'Healthshot hitmarker', 'Off', 'On hit', 'On kill')
 local slider = gui.Slider(msc_ref, 'lua_healthshot_hitmarker_slider', 'Healthshot duration (sec)', 1, 0, 10)
-
 
 local FOVenabled = gui.Checkbox(msc_ref, 'lua_healthshot_fovmarker_enabled', 'FOVmarker enabled', 0)
 local FovshotMode = gui.Combobox(msc_ref, 'lua_healthshot_fovmarker_combobox', 'Fovmarker', 'On hit', 'On kill')
 local sliderFOV = gui.Slider(msc_ref, 'lua_healthshot_fovmarker_duration', 'Animation duration (sec)', 1, 0, 10)
 local sliderSmooth = gui.Slider(msc_ref, 'lua_healthshot_fovmarker_smoothing', 'Animation smoothing', 80, 1, 100)
 local sliderStart = gui.Slider(msc_ref, 'lua_healthshot_fovmarker_startfov', 'Start FOV', 110, 50, 150)
-local sliderEnd = gui.Slider(msc_ref, 'lua_healthshot_fovmarkerr_endfov', 'End FOV', 120, 50, 150)
+local sliderEnd = gui.Slider(msc_ref, 'lua_healthshot_fovmarker_endfov', 'End FOV', 120, 50, 150)
+
+local CrossTime = 0
+local alpha = 0;
+local screenCenterX, screenCenterY = draw.GetScreenSize();
+screenCenterX = screenCenterX / 2;
+screenCenterY = screenCenterY / 2;
 
 listen('player_hurt')
 listen('player_death')
@@ -108,6 +117,10 @@ local function healthshot_hitmarker(e)
     if ((FOVenabled:GetValue()) and (FovshotMode:GetValue() == 0)) then
       add(slider:GetValue())
     end
+    if (hitcross:GetValue() == true) then
+      print("here1111")
+      CrossTime = globals.RealTime()
+    end
   end
 end
 
@@ -115,7 +128,7 @@ local function showFov(count, player)
   local nd = sliderEnd:GetValue()
   if globals.RealTime() < player.delay then
       if sliderStart:GetValue() > nd then
-        if player.fov > nd then player.fov = player.fov - (nd + player.fov) * sliderSmooth:GetValue()/20000 end -- время анимации
+        if player.fov > nd then player.fov = player.fov - (nd + player.fov) * sliderSmooth:GetValue()/1000 end -- время анимации
         if player.fov < nd then player.fov = nd end
         client.SetConVar("fov_cs_debug", player.fov, true)
       else
@@ -133,6 +146,21 @@ callbacks.Register('Draw', function()
   for index, hitfov in pairs(activeFovs) do
       showFov(index, hitfov)
   end
+  local step = 255 / 0.3 * globals.FrameTime()
+  local r,g,b,a = hitmarkerColor:GetValue()
+  if CrossTime + 0.4 > globals.RealTime() then
+     alpha = 255
+  else
+     alpha = alpha - step
+  end
+  if (alpha > 0) then
+    linesizeValue = linesize:GetValue()
+     draw.Color( r,g,b,alpha)
+     draw.Line( screenCenterX, screenCenterY - linesizeValue / 2, screenCenterX, screenCenterY - ( linesizeValue ))
+     draw.Line( screenCenterX - linesizeValue / 2, screenCenterY, screenCenterX - ( linesizeValue ), screenCenterY)
+     draw.Line( screenCenterX, screenCenterY + linesizeValue / 2, screenCenterX, screenCenterY + ( linesizeValue ))
+     draw.Line( screenCenterX + linesizeValue / 2, screenCenterY, screenCenterX + ( linesizeValue ), screenCenterY)
+  end
   if not allenabled:GetValue() then
     FovshotMode:SetInvisible( true )
     sliderFOV:SetInvisible( true )
@@ -140,6 +168,8 @@ callbacks.Register('Draw', function()
     sliderStart:SetInvisible( true )
     sliderEnd:SetInvisible( true )
     slider:SetInvisible( true )
+    hitcross:SetInvisible( true )
+    linesize:SetInvisible( true )
   else
     FovshotMode:SetInvisible( not FOVenabled:GetValue() )
     sliderFOV:SetInvisible( not FOVenabled:GetValue() )
@@ -151,6 +181,11 @@ callbacks.Register('Draw', function()
     else
       slider:SetInvisible( false )
     end
+    if hitcross:GetValue() == false then
+      linesize:SetInvisible( true )
+    else
+      linesize:SetInvisible( false )
+    end
   end
   killsay:SetInvisible( not allenabled:GetValue() )
   FOVenabled:SetInvisible( not allenabled:GetValue() )
@@ -158,4 +193,6 @@ callbacks.Register('Draw', function()
   hssounds:SetInvisible( not allenabled:GetValue() )
   hitssounds:SetInvisible( not allenabled:GetValue() )
   Healthshot:SetInvisible( not allenabled:GetValue() )
+  hitcross:SetInvisible( not allenabled:GetValue() )
+
 end);

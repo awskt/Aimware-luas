6cf0630de370d45922720538b7ef932edae807c2
iSunOfficial <38258637+iSunOfficial@users.noreply.github.com>
2020-08-23 14:59:24 +0500

    Update, added disable animation

--- SmoothTP.lua
+++ SmoothTP.lua
@@ -1,26 +1,60 @@
 local activeTP = {};
 local currentTP = gui.GetValue("esp.local.thirdpersondist" )
-local function add(time, ...)
-    table.insert(activeTP, {
-        ["time"] = time,
-        ["delay"] = globals.RealTime() + time,
-        ["tp"] = 0,
-    })
+local OnyxRef = gui.Reference("Visuals", "Local", "Camera")
+local tptggle;
+local OnyxFakeActivate = gui.Keybox(OnyxRef, tptggle, "Smoother TP", 6)
+local function add(time, yes, ...)
+    if yes then
+        table.insert(activeTP, {
+            ["time"] = time,
+            ["delay"] = globals.RealTime() + time,
+            ["tp"] = 0,
+            ["y"] = true,
+        })
+    else
+        table.insert(activeTP, {
+            ["time"] = time,
+            ["delay"] = globals.RealTime() + time,
+            ["tp"] = gui.GetValue("esp.local.thirdpersondist" )-1,
+            ["y"] = false,
+        })
+    end
 end
 local function showtp(count, player)
-    local nd = currentTP
-    if globals.RealTime() < player.delay then
-            if player.tp < nd then player.tp = player.tp + (nd - player.tp) * 0.08 end
+    if player.y then
+        local nd = currentTP
+        if globals.RealTime() < player.delay then
+                if player.tp < nd then player.tp = player.tp + (nd - player.tp) * 0.08 end
+                if player.tp > nd then player.tp = nd end
+                gui.SetValue("esp.local.thirdpersondist", player.tp )
+        else
+            table.remove(activeTP, count)
+        end
+    else
+        local nd = currentTP
+        if (player.tp > 0) then
+            if player.tp < nd then player.tp = player.tp - (nd + player.tp) * 0.02 end
             if player.tp > nd then player.tp = nd end
             gui.SetValue("esp.local.thirdpersondist", player.tp )
-    else
+        else
+            gui.SetValue("esp.local.thirdperson", false )
             table.remove(activeTP, count)
+        end
     end
 end
-callbacks.Register('Draw', function()
-    if (input.IsButtonPressed(gui.GetValue("esp.local.thirdpersonkey" ))) then
-        add(1)
+local function addc()
+    if gui.GetValue( "esp.local.thirdperson" ) then
+        add(1, false)
+    else
+        gui.SetValue("esp.local.thirdperson", true )
+        add(1, true)
     end
+    
+end
+callbacks.Register('Draw', function()
+    if (input.IsButtonPressed(OnyxFakeActivate:GetValue())) then
+    addc();
+end
     for index, hittp in pairs(activeTP) do
         showtp(index, hittp)
     end
